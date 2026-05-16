acdd4efcea7336109f626cdea52c7658d1d7915a
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-05-16 18:47:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// your code here
+const name=document.getElementById("name");
+const year=document.getElementById("year");
+const btn=document.getElementById("button");
+const heading=document.getElementById("url");
+btn.addEventListener("click",()=>{
+	heading.innerHTML=`https://localhost:8080/?year=${year.value}`
+})
